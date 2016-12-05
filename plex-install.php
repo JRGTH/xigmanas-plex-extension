18644042de78a8b8edd60af9d3e21313b5169ebc
JRGTH <joserprg@gmail.com>
2016-12-05 07:32:14 -0400

    Fix installer and text edits

--- plex-install.php
+++ plex-install.php
@@ -39,16 +39,13 @@
 require("auth.inc");
 require("guiconfig.inc");
 
-// Predefined functions overrides.
-function gtxt($data) {
-	return htmlspecialchars(gettext($data), ENT_QUOTES);
-}
-
 $application = "Plex Media Server";
-$pgtitle = array(gtxt("Extensions"), gtxt($application), gtxt("Installation Directory"));
+$pgtitle = array(gtext("Extensions"), gtext($application), gtext("Installation Directory"));
 if (!isset($config['plex']) || !is_array($config['plex'])) $config['plex'] = array();
 $date = strftime('%c');
 $logfile = "plex_ext.log";
+$branch = "testing";
+$git_url = "https://raw.githubusercontent.com/JRGTH/nas4free-plex-extension/{$branch}/plex/plexinit";
 
 /*
 Check if the directory exists, the mountpoint has at least o=rx permissions and
@@ -57,28 +54,28 @@ set the permission to 775 for the last directory in the path.
 function change_perms($dir) {
 	global $input_errors;
 
-	$path = rtrim($dir,'/');   // Remove trailing slash.
+	$path = rtrim($dir,'/');											// Remove trailing slash.
 	if (strlen($path) > 1) {
-		if (!is_dir($path)) {   // Check if directory exists.
-			$input_errors[] = sprintf(gtxt("Directory %s doesn't exist!"), $path);
+		if (!is_dir($path)) {											// Check if directory exists.
+			$input_errors[] = sprintf(gtext("Directory %s doesn't exist!"), $path);
 		}
 		else {
-			$path_check = explode("/", $path);   // Split path to get directory names.
-			$path_elements = count($path_check);   // Get path depth.
-			$fp = substr(sprintf('%o', fileperms("/$path_check[1]/$path_check[2]")), -1);   // Get mountpoint permissions for others.
-			if ($fp >= 5) {   // Some  applications needs at least read & search permission at the mountpoint.
-				$directory = "/$path_check[1]/$path_check[2]";   // Set to the mountpoint.
-				for ($i = 3; $i < $path_elements - 1; $i++) {   // Traverse the path and set permissions to rx.
-					$directory = $directory."/$path_check[$i]";   // Add next level.
-					exec("chmod o=+r+x \"$directory\"");   // Set permissions to o=+r+x.
+			$path_check = explode("/", $path);							// Split path to get directory names.
+			$path_elements = count($path_check);						// Get path depth.
+			$fp = substr(sprintf('%o', fileperms("/$path_check[1]/$path_check[2]")), -1);	// Get mountpoint permissions for others.
+			if ($fp >= 5) {												// Some  applications needs at least read & search permission at the mountpoint.
+				$directory = "/$path_check[1]/$path_check[2]";			// Set to the mountpoint.
+				for ($i = 3; $i < $path_elements - 1; $i++) {			// Traverse the path and set permissions to rx.
+					$directory = $directory."/$path_check[$i]";			// Add next level.
+					exec("chmod o=+r+x \"$directory\"");				// Set permissions to o=+r+x.
 				}
 				$path_elements = $path_elements - 1;
-				$directory = $directory."/$path_check[$path_elements]";   // Add last level.
-				exec("chmod 775 {$directory}");   // Set permissions to 775.
+				$directory = $directory."/$path_check[$path_elements]";	// Add last level.
+				exec("chmod 775 {$directory}");							// Set permissions to 775.
 				exec("chown {$_POST['who']} {$directory}*");
 			}
 			else {
-				$input_errors[] = sprintf(gtxt("%s needs at least read & execute permissions at the mount point for directory %s! Set the Read and Execute bits for Others (Access Restrictions | Mode) for the mount point %s (in <a href='disks_mount.php'>Disks | Mount Point | Management</a> or <a href='disks_zfs_dataset.php'>Disks | ZFS | Datasets</a>) and hit Save in order to take them effect."), $application, $path, "/{$path_check[1]}/{$path_check[2]}");
+				$input_errors[] = sprintf(gtext("%s needs at least read & execute permissions at the mount point for directory %s! Set the Read and Execute bits for Others (Access Restrictions | Mode) for the mount point %s (in <a href='disks_mount.php'>Disks | Mount Point | Management</a> or <a href='disks_zfs_dataset.php'>Disks | ZFS | Datasets</a>) and hit Save in order to take them effect."), $application, $path, "/{$path_check[1]}/{$path_check[2]}");
 			}
 		}
 	}
@@ -88,19 +85,19 @@ if (isset($_POST['save-install']) && $_POST['save-install']) {
 	unset($input_errors);
 	if (empty($input_errors)) {
 		$config['plex']['storage_path'] = !empty($_POST['storage_path']) ? $_POST['storage_path'] : $g['media_path'];
-		$config['plex']['storage_path'] = rtrim($config['plex']['storage_path'],'/');   // Ensure to have NO trailing slash.
+		$config['plex']['storage_path'] = rtrim($config['plex']['storage_path'],'/');	// Ensure to have NO trailing slash.
 		if (!isset($_POST['path_check']) && (strpos($config['plex']['storage_path'], "/mnt/") === false)) {
-			$input_errors[] = gtxt("The common directory for extensions MUST be set to a directory below '/mnt/' to prevent to loose the extension after a reboot on embedded systems!");
+			$input_errors[] = gtext("The common directory for extensions MUST be set to a directory below '/mnt/' to prevent to loose the extension after a reboot on embedded systems!");
 		}
 		else {
 			if (!is_dir($config['plex']['storage_path'])) mkdir($config['plex']['storage_path'], 0775, true);
 			change_perms($config['plex']['storage_path']);
 			$config['plex']['path_check'] = isset($_POST['path_check']) ? true : false;
-			$install_dir = $config['plex']['storage_path']."/";   // Get directory where the installer script resides.
+			$install_dir = $config['plex']['storage_path']."/";	// Get directory where the installer script resides.
 			if (!is_dir("{$install_dir}plex/log")) { mkdir("{$install_dir}plex/log", 0775, true); }
-			$return_val = mwexec("fetch {$verify_hostname} -vo {$install_dir}plex/plexinit 'https://raw.githubusercontent.com/JRGTH/nas4free-plex-extension/testing/plex/plexinit'", true);
+			$return_val = mwexec("fetch -avo {$install_dir}plex/plexinit --no-verify-peer --timeout=30 {$git_url}", true);
 			if ($return_val == 0) {
-				// Perform cleanup for obsolete files on upgrades.
+				// Perform cleanup for obsolete files on upgrade/reinstall.
 				if (is_file("plex-gui.php")) {
 					if (is_file("{$install_dir}plex/version")) unlink("{$install_dir}plex/version");
 					if (is_dir("{$install_dir}plex/conf")) exec("rm -rf {$install_dir}plex/conf");
@@ -113,10 +110,10 @@ if (isset($_POST['save-install']) && $_POST['save-install']) {
 				exec("sh {$install_dir}plex/plexinit -o");
 				exec("php {$install_dir}plex/postinit");
 				if (is_file("{$install_dir}plex/postinit")) unlink("{$install_dir}plex/postinit");
-				exec("echo '{$date}: {$application} Extension successfully installed' > {$install_dir}plex/log/{$logfile}");
+				exec("echo '{$date}: {$application} extension successfully installed' > {$install_dir}plex/log/{$logfile}");
 			}
 			else {
-				$input_errors[] = sprintf(gtxt("Installation file %s not found, installation aborted!"), "{$install_dir}plex/plexinit");
+				$input_errors[] = sprintf(gtext("Installation file %s not found, installation aborted!"), "{$install_dir}plex/plexinit");
 				return;
 			}
 			mwexec("rm -rf ext/plex-install; rm -f plex-install.php", true);
@@ -128,7 +125,7 @@ if (isset($_POST['save-install']) && $_POST['save-install']) {
 if (isset($_POST['cancel']) && $_POST['cancel']) {
 	$return_val = mwexec("rm -rf ext/plex-install; rm -f plex-install.php", true);
 	if (is_dir("ext")) exec("if [ ! $(ls -A ext) ]; then rm -r ext; fi");
-	if ($return_val == 0) { $savemsg .= $application." ".gtxt("not installed"); }
+	if ($return_val == 0) { $savemsg .= $application." ".gtext("not installed"); }
 	else { $input_errors[] = $application." removal failed"; }
 	header("Location:index.php");
 }
@@ -147,12 +144,12 @@ include("fbegin.inc"); ?>
 		<?php if (!empty($savemsg)) print_info_box($savemsg);?>
 		<table width="100%" border="0" cellpadding="6" cellspacing="0">
 			<?php html_titleline($application);?>
-			<?php html_filechooser("storage_path", gtxt("Common directory"), $pconfig['storage_path'], gtxt("Common directory for the Plex Extension, a persistent place where the extension should installed, a directory below /mnt/."), $pconfig['storage_path'], true, 60);?>
-			<?php html_checkbox("path_check", gtxt("Path check"), $pconfig['path_check'], gtxt("If this option is selected no examination of the common directory path will be carried out, whether it was set to a directory below /mnt/."), "<b><font color='red'>".gtxt("Please use this option only if you know what you are doing!")."</font></b>", false);?>
+			<?php html_filechooser("storage_path", gtext("Common directory"), $pconfig['storage_path'], gtext("Common directory for the Plex Extension, a persistent place where the extension should installed, a directory below /mnt/."), $pconfig['storage_path'], true, 60);?>
+			<?php html_checkbox("path_check", gtext("Path check"), $pconfig['path_check'], gtext("If this option is selected no examination of the common directory path will be carried out, whether it was set to a directory below /mnt/."), "<b><font color='red'>".gtext("Please use this option only if you know what you are doing!")."</font></b>", false);?>
 		</table>
 		<div id="submit">
-			<input id="save-install" name="save-install" type="submit" class="formbtn" value="<?=gtxt("Save & Install");?>" onclick="return confirm('<?=gtxt("Ready to install Plex Media Server Extension?");?>')" />
-			<input id="cancel" name="cancel" type="submit" class="formbtn" value="<?=gtxt("Cancel");?>"/>
+			<input id="save-install" name="save-install" type="submit" class="formbtn" value="<?=gtext("Save & Install");?>" onclick="return confirm('<?=gtext("Ready to install Plex Media Server Extension?");?>')" />
+			<input id="cancel" name="cancel" type="submit" class="formbtn" value="<?=gtext("Cancel");?>"/>
 		</div>
 	</td></tr>
 	</table>
--- plex-install.sh
+++ plex-install.sh
@@ -12,62 +12,62 @@ SCRIPTNAME=`basename $0`
 
 error_notify()
 {
-    # Logg and notify message on error and exit.
-    /usr/bin/logger "${SCRIPTNAME}: an error has occurred during install process"
-    echo -e "$*" >&2 ; exit 1
+	# Logg and notify message on error and exit.
+	/usr/bin/logger "${SCRIPTNAME}: an error has occurred during install process"
+	echo -e "$*" >&2 ; exit 1
 }
 
 fetch_branch()
 {
-    # Fetch latest master branch, unless other specified.
-    if [ "${BRANCH}" = "testing" ]; then
-        # Fetch the latest testing branch(for developers/testing).
-        echo "=> Retrieving the latest testing branch..."
-        /usr/bin/fetch -o ${CWDIR} https://github.com/JRGTH/nas4free-plex-extension/archive/testing.zip || error_notify "Error: A problem has occurred while fetching testing branch."
-        /bin/mv ${CWDIR}/testing.zip ${CWDIR}/master.zip
-    else
-        # Fetch the latest master branch.
-        echo "=> Retrieving the latest master branch..."
-        /usr/bin/fetch -o ${CWDIR} https://github.com/JRGTH/nas4free-plex-extension/archive/master.zip || error_notify "Error: A problem has occurred while fetching master branch."
-    fi
+	# Fetch latest master branch, unless other specified.
+	if [ "${BRANCH}" = "testing" ]; then
+		# Fetch the latest testing branch(for developers/testing).
+		echo "=> Retrieving the latest testing branch..."
+		/usr/bin/fetch -o ${CWDIR} https://github.com/JRGTH/nas4free-plex-extension/archive/testing.zip || error_notify "Error: A problem has occurred while fetching testing branch."
+		/bin/mv ${CWDIR}/testing.zip ${CWDIR}/master.zip
+	else
+		# Fetch the latest master branch.
+		echo "=> Retrieving the latest master branch..."
+		/usr/bin/fetch -o ${CWDIR} https://github.com/JRGTH/nas4free-plex-extension/archive/master.zip || error_notify "Error: A problem has occurred while fetching master branch."
+	fi
 }
 
 install_main()
 {
-    # Check for system/product compatibility.
-    if [ ! ${PLATFORM} == "amd64" ]; then
-        echo "Unsupported platform!"; exit 1
-    fi
-    if [ ! ${PRDVERSION} -ge "10" ]; then
-        echo "Unsupported version!"; exit 1
-    fi
+	# Check for system/product compatibility.
+	if [ ! ${PLATFORM} == "amd64" ]; then
+		echo "Unsupported platform!"; exit 1
+	fi
+	if [ ! ${PRDVERSION} -ge "10" ]; then
+		echo "Unsupported version!"; exit 1
+	fi
 
-    # Fetch selected branch, default master.
-    fetch_branch
+	# Fetch selected branch, default master.
+	fetch_branch
 
-    # Extract the package and exclude unneeded files.
-    echo "=> Extracting package files..."
-    /usr/bin/tar -xf ${CWDIR}/master.zip --exclude='.git*' --strip-components 1 -C ${CWDIR}/ || error_notify "Error: A problem has occurred while extracting package."
+	# Extract the package and exclude unneeded files.
+	echo "=> Extracting package files..."
+	/usr/bin/tar -xf ${CWDIR}/master.zip --exclude='.git*' --strip-components 1 -C ${CWDIR}/ || error_notify "Error: A problem has occurred while extracting package."
+	/bin/rm -f "${CWDIR}/master.zip" "${CWDIR}/README.md" "${CWDIR}/plex-install.php" "${CWDIR}/plex-install.sh"
 
-    # Make executable and run plexinit script.
-    /bin/chmod 0755 ${CWDIR}/plex/plexinit
-    ${CWDIR}/plex/plexinit
+	# Make executable and run plexinit script.
+	/bin/chmod 0755 ${CWDIR}/plex/plexinit
+	${CWDIR}/plex/plexinit
 
-    # Check exit status and finish installation.
-    if [ $? -eq 0 ]; then
-        echo "Plex Extension installed successfully!"
-        /usr/bin/logger "${SCRIPTNAME} Plex Extension installed successfully"
-        /bin/rm -f "${CWDIR}/master.zip" "${CWDIR}/README.md" "${CWDIR}/plex-install.sh"
-    else
-        error_notify "Error: A problem has occurred during install process."
-    fi
+	# Check exit status and finish installation.
+	if [ $? -eq 0 ]; then
+		echo "Plex Extension installed successfully!"
+		/usr/bin/logger "${SCRIPTNAME} Plex Extension installed successfully"
+	else
+		error_notify "Error: A problem has occurred during install process."
+	fi
 }
 
 # Alternate branches(for developers/testing).
 while getopts ":t" option; do
-    case ${option} in
-        [t]) BRANCH="testing";;
-    esac
+	case ${option} in
+		[t]) BRANCH="testing";;
+	esac
 done
 
 install_main
